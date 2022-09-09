d65838e16d6d8a11d6a5bcd58d52abe1ff4ba903
Kevwoda Lucky <ajesamuel0@gmail.com>
2022-09-09 12:46:36 -0700

    Adding 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - serves as the entry point of the program.
+ * Return: By default returns zero if no errors otherwise return non-zero.
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
