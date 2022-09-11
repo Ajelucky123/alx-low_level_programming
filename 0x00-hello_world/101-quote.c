7be80912e37adc1a2754eff855f74aabfaefe67a
Kevwoda Lucky <ajesamuel0@gmail.com>
2022-09-10 18:16:08 -0700

    Adding quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 /**
- * main - entry point of the program.
+ * main - Entry point of the program.
  *
- * Return: return 1 if success otherwise return non-zero if error.
+ * Return: Always 0 (success)
  */
 int main(void)
 {
+
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+	
 	return (1);
 }
