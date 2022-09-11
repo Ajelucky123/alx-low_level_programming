699d7f517260cb5a3f01a12ca452cc609fa744b6
Kevwoda Lucky <ajesamuel0@gmail.com>
2022-09-10 17:25:27 -0700

    Adding 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,15 +1,21 @@
 #include <stdio.h>
 /**
- * main- entry point for the program.
+ * main - entry point for the program.
  *
  * Return: 0 if success,non-zero if error.
  */
 int main(void)
 {
-	printf("Size of a char: l%d byte(s)\n". sizeof(char));
-	printf("Size of an int: l%d byte(s)\n", sizeof(int));
-	printf("Size of a long int: l%d byte(s)\n", sizeof(long int));
-	printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
-	printf("Size of a float: %ld byte(s)\n", sizeof(float));
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
+
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long) sizeof(b));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 	return (0);
 }
